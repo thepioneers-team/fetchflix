18830f68755adb6d792bbb2509c5d0682d7e4bce
Hosein <48409432+Ho3einWave@users.noreply.github.com>
2024-02-11 03:54:58 +0330

    feat: background is done

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,17 +1,21 @@
-import './assets/base.css'
+import "./assets/base.css";
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-import { NextUIProvider } from '@nextui-org/react'
+import { NextUIProvider } from "@nextui-org/react";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <NextUIProvider>
-      {/* <main className="dark h-screen text-foreground bg-[#1D1D1D]"> */}
-      <App />
-      {/* </main> */}
+      <main className="h-screen bg-[#1D1D1D] text-foreground dark">
+        <div className="absolute -top-16 left-4 h-[30rem] w-[30rem] rounded-full bg-[#2DDFC770]"></div>
+        <div className="absolute left-0 top-0 z-10 h-full w-full backdrop-blur-[300px]"></div>
+        <div className="relative z-10">
+          <App />
+        </div>
+      </main>
     </NextUIProvider>
-  </React.StrictMode>
-)
+  </React.StrictMode>,
+);
