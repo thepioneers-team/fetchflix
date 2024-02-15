1b1ff2c1ac43aaf896c6dcde535537aa788e7452
SkylerX <hello@skylerx.ir>
2024-02-14 19:33:20 -0500

    moved formats to a constants file

--- src/renderer/src/components/QualitySelector.tsx
+++ src/renderer/src/components/QualitySelector.tsx
@@ -5,89 +5,14 @@ import {
   PopoverTrigger,
   cn,
 } from "@nextui-org/react";
+import { FORMATS } from "@renderer/utils/constants";
 import { useState } from "react";
 
 import { PiFileAudioBold } from "react-icons/pi";
 import { RiFileVideoLine } from "react-icons/ri";
 
-const FORMATS = [
-  {
-    isVideo: true,
-    value: "3GP",
-  },
-  {
-    isVideo: false,
-    value: "AAC",
-  },
-  {
-    isVideo: false,
-    value: "FLAC",
-  },
-  {
-    isVideo: true,
-    value: "FLV",
-  },
-  {
-    isVideo: true,
-    value: "M4A",
-  },
-  {
-    isVideo: false,
-    value: "MP3",
-  },
-  {
-    isVideo: true,
-    value: "MP4",
-  },
-  {
-    isVideo: false,
-    value: "OPUS",
-  },
-  {
-    isVideo: false,
-    value: "VORBIS",
-  },
-  {
-    isVideo: false,
-    value: "WAV",
-  },
-  {
-    isVideo: true,
-    value: "WEBM",
-  },
-  {
-    isVideo: true,
-    value: "BEST",
-  },
-  {
-    isVideo: true,
-    value: "WORST",
-  },
-  { separator: true },
-  {
-    isVideo: true,
-    larger: true,
-    value: "BESTVIDEO",
-  },
-  {
-    isVideo: true,
-    larger: true,
-    value: "BESTAUDIO",
-  },
-  {
-    isVideo: true,
-    larger: true,
-    value: "BESTVIDEO+BESTAUDIO",
-  },
-  {
-    isVideo: true,
-    larger: true,
-    value: "BESTVIDEO[EXT=MP4]+BESTAUDIO[EXT=M4A]/BEST[EXT=MP4]/BEST",
-  },
-];
-
 const QualitySelector = () => {
-  const [selectedValue, setSelectedValue] = useState<string | null>("hello");
+  const [selectedValue, setSelectedValue] = useState<string | null>("BEST");
 
   const handleValueChange = (value: string) => {
     setSelectedValue(value);
